1d1f5cb1840c615be6aeef4de3d9001a1c6ebf48
Ram Surya <ramsurya0917@gamil.com>
2020-05-23 00:34:30 +0530

    few updates on learnt items in python

--- Python Basic/exx01.py
+++ Python Basic/exx01.py
@@ -1,5 +1,19 @@
 import sys
+import logging 
+import os
 
-print("hello")
-sys.exit()
-print('goodbye')
+logging.basicConfig(filename ='log.txt',level = logging.DEBUG ,format = '%(asctime)s - %(levelname)s -%(message)s')
+
+fac =1
+logging.debug('Start debug')
+def factorial(n):
+    global fac        
+    fac = n * fac
+    logging.debug('this is n %s and this fac %s'%(n,fac))
+    if n == 1:
+        return fac
+    return factorial(n-1)
+
+print(factorial(4))
+
+os.getcwd()
